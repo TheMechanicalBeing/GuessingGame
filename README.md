3c32d3c6812fbd783d745089caa823566e93da14
TheMechanicalBeing <tornike.tsulukidze@gmail.com>
2023-10-05 02:17:18 +0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,20 +1,7 @@
 # C
 
-This is my first C project.
+This is simple guessing game project
 
-It's easy to code and implement this project.
+If winning is the goal the user has to guess NUMBER_COUNT of numbers
 
-But remember, you become great by solving little parts together.
-
-### About Project
-
-The project is console application, about Store that "stores" and sells its products.
-
-When you run the code you can use commands:
-
-* 'h' - outputs description about commands information
-* 'a' - lets user add product's name and price
-* 'l' - lists store items with its index, product name and price
-* 'p' - lets use to purchase product considering item's price (in USD) and quantities
-* 't' - outputs information about how much money did user spend
-* 'q' - quits application
+Instructions and idea is simple but implementing code on this language is short but fun and important experience.
--- main.c
+++ main.c
@@ -1,87 +1,142 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-struct Store {
-    char product[100];
-    unsigned int price;
+// Change constants for different manipulations
+#define NUMBERS_COUNT 10
+#define MIN_NUMBER 1
+#define MAX_NUMBER 200
+#define INITIAL_SIZE 5
+
+typedef unsigned int positive;
+
+struct object {
+    positive number;
+    char show_index;
 };
 
-struct Store products[100];
-unsigned int productCount = 0, totalSpend;
 
-void printCommands() {
-    printf("\nList of available commands:\n"
-           "'h' - outputs this help information\n"
-           "'a' - adds new product to database\n"
-           "'l' - lists all products in database\n"
-           "'p' - enters purchase mode\n"
-           "'t' - outputs total money spent by user\n"
-           "'q' - quits program\n\n");
+// Generating NUMBERS_COUNT of random numbers and storing it into the pointer
+positive *generate_numbers() {
+    positive *to_return = (positive *) malloc(sizeof(positive) * NUMBERS_COUNT);
+
+    for (int i = 0; i < NUMBERS_COUNT; i++) {
+        to_return[i] = rand() % (MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER;
+    }
+
+    return to_return;
 }
 
-void addProduct() {
-    printf("Product name:");
-    scanf("%s", products[productCount].product);
-    printf("Product's unit price:");
-    scanf("%u", &products[productCount].price);
-    printf("Product added successfully\n");
-    productCount++;
+
+void fill_object(struct object to_fill[], const positive * source) {
+    for (int i = 0; i < NUMBERS_COUNT; i++) {
+        to_fill[i].number = source[i];
+        to_fill[i].show_index = 0;
+    }
 }
 
-void listProducts() {
+
+// checking if target is inside to_fill[]
+char check_answer(struct object to_fill[], positive target) {
+    for (int i = 0; i < NUMBERS_COUNT; i++) {
+        if (to_fill[i].number == target && to_fill[i].show_index == 0) {
+            to_fill[i].show_index = 1;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+void print_object(struct object to_print[]) {
+    for (int i = 0; i < NUMBERS_COUNT; i++) {
+        if (to_print[i].show_index)
+            printf("%d ", to_print[i].number);
+        else{
+            positive temp = to_print[i].number;
+            while (temp != 0) {
+                printf("_");
+                temp /= 10;
+            }
+            printf(" ");
+        }
+    }
     printf("\n");
-    for (int i = 0; i < productCount; i++) {
-        printf("Product %d\n"
-               "\tName: %s\n"
-               "\tPrice: %u USD\n", i, products[i].product, products[i].price);
-    }
 }
 
-void purchaseProducts() {
-    unsigned int index, quantity, spend;
-    printf("Enter product index:");
-    scanf("%u", &index);
 
-    if (index >= 0 && index <= productCount-1 && 0 != productCount) {
-        printf("You are going to purchase %s which costs %d USD. What is the quantity you are looking for?",
-               products[index].product, products[index].price);
-        scanf("%u", &quantity);
-        spend = quantity * products[index].price;
-        totalSpend += spend;
-        printf("\nProduct purchased successfully. You spent %d USD on it\n", spend);
-    } else {
-        printf("Product with given index does not exist\n");
+// checking if user guessed all the numbers
+char win_condition(struct object obj[]) {
+    for (int i = 0; i < NUMBERS_COUNT; i++) {
+        if (!obj[i].show_index)
+            return 0;
     }
+    return 1;
 }
+
 
 int main() {
+    // Generating seed random from current time
+    srand(time(NULL));
 
-    char command = '_';
-    printf("Type 'h' to list available commands\n");
+    printf("----------Numbers Game-----------\n"
+           "Think of %d numbers from %d..%d range. Try to guess them\n"
+           "Input 'q' in order to exit and show results\n", NUMBERS_COUNT, MIN_NUMBER, MAX_NUMBER);
 
-    while (command != 'q') {
-        scanf("%c", &command);
+    positive * generated = generate_numbers();
+    struct object numbers_to_guess[NUMBERS_COUNT];
+    fill_object(numbers_to_guess, generated);
 
-        switch (command) {
-            case 'h':
-                printCommands();
-                break;
-            case 'a':
-                addProduct();
-                break;
-            case 'l':
-                listProducts();
-                break;
-            case 'p':
-                purchaseProducts();
-                break;
-            case 't':
-                printf("Total money spend is %d\n", totalSpend);
-                break;
-            default:
-                continue;
+    char *input = (char *) malloc(sizeof(char) * INITIAL_SIZE);
+    int current_size = INITIAL_SIZE;
+
+    while (1) {
+        print_object(numbers_to_guess);
+
+        printf("Your input:");
+        scanf("%s", input);
+        if (input[0] == 'q') {
+            printf("The right answer was\n");
+            int tmp = 0;
+            for (int i = 0; i < NUMBERS_COUNT; i++) {
+                printf("%d ", numbers_to_guess[i].number);
+                if (numbers_to_guess[i].show_index == 1)
+                    tmp++;
+            }
+            printf("You guessed %d numbers\n", tmp);
+            break;
+        }
+
+        positive current_input = atoi(input);
+        if (current_input < MIN_NUMBER || current_input > MAX_NUMBER)
+            printf("The range is %d..%d\n", MIN_NUMBER, MAX_NUMBER);
+        else {
+            if (check_answer(numbers_to_guess, current_input))
+                printf("Good Job!\n");
+            else
+                printf("Close Enough!\n");
+        }
+
+        if (win_condition(numbers_to_guess)) {
+            printf("Congrats you won this hard game!\n");
+            break;
+        }
+        // Increasing memory size of input if length is exceeded
+        if (strlen(input) + 1 >= current_size) {
+            input = (char *) realloc(input, strlen(input) + 1);
+
+            if (input == NULL) {
+                fprintf(stderr, "There was a memory error\n");
+                free(input);
+                free(generated);
+                return 1;
+            }
         }
 
     }
 
+    free(generated);
+    free(input);
     return 0;
 }
